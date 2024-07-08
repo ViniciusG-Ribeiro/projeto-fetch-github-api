e87af93e78d035118888102deb9b45ad3f5969e8
ViniciusG.Ribeiro <viniciusrib2014@hotmail.com>
2024-07-08 13:11:28 -0300

    Adicionado sessao evento e dados dos repositorios

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -7,13 +7,34 @@ const screen = {
                                 <h1>${user.name ?? 'Não possui nome cadastrado 😭'}</h1>
                                 <p>${user.bio ?? 'Não possui bio cadastrada 😭'}</p>
                                 <br>
+                                <i class="fas fa-users"></i>
                                 <h3>Followers: ${user.followers ?? 'Não tem seguidores 🐺'} </h3>
-                                <h3>Following: ${user.following ?? 'Não segue ninguem'} </h3>
+                                <h3>Following: ${user.following ?? 'Não segue ninguem'} </h3>                             
                             </div>
                         </div>`
 
+        let EventsItens = ''
+        let i = 1;
+        user.events.forEach(event => {
+            if(i <= 10 & event.type === 'PushEvent' || i <=10 & event.type === 'CreateEvent'){
+                EventsItens += `<li><p style="font-weight: 800;">${event.repo.name}: &nbsp;&nbsp; <span>- ${event.payload.description ?? 'Sem mensagem de commit'}</span></p></li>`;
+                i++
+            }
+        })
+
+        if(EventsItens !==''){
+            this.userProfile.innerHTML += `<div>
+                                            <h2>Eventos</h2>
+                                            <br>
+                                            <ul>${EventsItens}</ul>
+                                            </div>`
+        }
+
         let repositoriesItens = ''
-        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
+        user.repositories.forEach(repo => repositoriesItens += `<li>
+            <a href="${repo.html_url}" target="_blank">${repo.name} 
+            <br> <br>
+            <span>🍴${repo.forks_count}</span> <span>⭐${repo.stargazers_count}</span> <span>👀${repo.watchers_count}</span> <span>🧑‍💻 ${repo.language}</span></a></li>`)
 
         if(user.repositories.length > 0){
             this.userProfile.innerHTML += `<div class="repositories section">
@@ -26,7 +47,5 @@ const screen = {
         this.userProfile.innerHTML = '<h3>Usuário não encontrado!</h3>'
     }
 }
-
-
 
 export { screen }
